78ba96a964be63e00db5d345676d5ba441d0bf67
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2019-04-24 10:32:50 +0200

    feat: prune unused volumes

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -4,3 +4,4 @@ DOCKER_BIN=$(which docker)
 
 if [[ $($DOCKER_BIN ps -a -q | wc -l) -ne 0 ]]; then $DOCKER_BIN rm $($DOCKER_BIN ps -a -q); fi
 if [[ $($DOCKER_BIN images -q | wc -l) -ne 0 ]]; then $DOCKER_BIN rmi $($DOCKER_BIN images -q); fi
+$DOCKER_BIN system prune --force --all --volumes
