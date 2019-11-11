627395953315c6cbb6cff15400e1754f407ab12c
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2019-11-11 12:55:52 +0100

    Basic disc cleanup (xcode, yarn, ...)

---
+++ disk-cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Delete old simulators
+xcrun simctl delete unavailable
+
+# Delete old app archives
+rm -rf ~/Library/Developer/Xcode/Archives
+
+# Delete old logs
+cd ~/Library/Logs/CoreSimulator
+rm -rf *
+
+# Cleanup DerivedData
+rm -rf ~/Library/Developer/Xcode/DerivedData
+
+# Remove yarn cache
+yarn cache clean
+
+# Look into ~/Library/Developer/Xcode/iOS DeviceSupport for old versions
+
+# Find other places with ncdu
