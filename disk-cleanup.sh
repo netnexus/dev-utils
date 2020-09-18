a80a36717fb0fa95047eade964a5f99451fdcdbe
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2020-09-18 10:52:12 +0200

    Clear TypeScript and Brew cache

--- disk-cleanup.sh
+++ disk-cleanup.sh
@@ -13,8 +13,14 @@ rm -rf *
 # Cleanup DerivedData
 rm -rf ~/Library/Developer/Xcode/DerivedData
 
+# TypeScript cache
+rm -rf ~/Library/Caches/typescript
+
 # Remove yarn cache
 yarn cache clean
+
+# Clear brew cache
+/usr/local/bin/brew cleanup -s
 
 # Look into ~/Library/Developer/Xcode/iOS DeviceSupport for old versions
 
